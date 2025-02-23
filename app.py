f2a07f2f8c230554dc5cc1f6c6f21649ceb737c1
bigbox1304 <123052330+bigbox1304@users.noreply.github.com>
2025-02-23 23:33:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,5 +94,6 @@ def quiz():
     
     return redirect(url_for("index"))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Lấy PORT từ biến môi trường, mặc định là 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
